60bc4a19d44e840ce8e54b1b2fee6f46c1e519ac
Gabi <gabriellec9@outlook.com>
2025-10-31 16:27:00 +0000

    2

--- pre_leetcode/2-score-of-a-string/dungeon1/character-values.py
+++ pre_leetcode/2-score-of-a-string/dungeon1/character-values.py
@@ -1,7 +1,3 @@
 # Dungeon 1 — Character Values
 # Goal: Get a single character from the user and print its ASCII value
 # TODO: print the ASCII value of char using ord()
-
-char = input("Enter a single character: ")
-
-print(ord(char))
---
+++ pre_leetcode/answers/2-score-of-a-string.py
@@ -0,0 +1,11 @@
+#DUNGEON 1
+
+char = input("Enter a single character: ")
+print(ord(char))
+
+
+#DUNGEON 2
+string = input("Enter a short string: \n")
+
+for c in range(len(string)):
+  print(ord(string[c]))
