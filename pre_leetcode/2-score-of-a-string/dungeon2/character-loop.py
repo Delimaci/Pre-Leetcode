23e6db895d61556b2be5138637a211db668522eb
Gabi <gabriellec9@outlook.com>
2025-10-31 16:25:31 +0000

    Create character-loop.py

---
+++ pre_leetcode/2-score-of-a-string/dungeon2/character-loop.py
@@ -0,0 +1,13 @@
+# Dungeon 2 — Character Loop Intro ⚔️
+# Goal: Loop through each character of a string and examine it
+# Task 1: Ask the user to enter a short string
+# Task 2: Loop through each character in the string
+# Task 3: For each character, find its ASCII value using ord()
+# Task 4: Print the character along with its ASCII value
+# Purpose: Teach iterating over strings and using ord() in preparation for calculating differences between ASCII values
+
+
+string = input("Enter a short string: \n")
+
+for c in range(len(string)):
+  print(ord(string[c]))
